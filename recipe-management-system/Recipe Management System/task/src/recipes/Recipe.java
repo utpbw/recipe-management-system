6d0d2606441ccdf6867a5521369e3bf753c28271
Gottfried Schneider <forum-gs-mac-air-it@dd3yp-12.hwh.whka.de>
2025-02-15 20:45:34 +0100

    Recipe Management System - Stage 3

--- recipe-management-system/Recipe Management System/task/src/recipes/Recipe.java
+++ recipe-management-system/Recipe Management System/task/src/recipes/Recipe.java
@@ -1,13 +1,27 @@
 package recipes;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
 
 @Data
+@NoArgsConstructor
 public class Recipe {
 
+    @JsonIgnore
+    private long id;  // ID should be assigned externally
     private String name;
     private String description;
-    private String ingredients;
-    private String directions;
+    private List<String> ingredients;
+    private List<String> directions;
+
+    public Recipe(String name, String description, List<String> ingredients, List<String> directions) {
+        this.name = name;
+        this.description = description;
+        this.ingredients = ingredients;
+        this.directions = directions;
+    }
 }
 
