2cdb45f59c987e05f4b84ffd2b0d72ed231a965a
Gottfried Schneider <forum-gs-mac-air-it@dd3yp-12.hwh.whka.de>
2025-02-15 19:12:12 +0100

    Recipe Management System - Stage 1

---
+++ recipe-management-system/Recipe Management System/task/src/recipes/Api.java
@@ -0,0 +1,20 @@
+package recipes;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class Api {
+
+    private Recipe recipe;
+
+    @PostMapping(value = "/recipe", consumes = "application/json")
+    public void addRecipe(@RequestBody Recipe recipe) {
+        this.recipe = recipe;
+    }
+
+    @GetMapping("/recipe")
+    public Recipe getRecipe() {
+        return recipe;
+    }
+}
---
+++ recipe-management-system/Recipe Management System/task/src/recipes/Recipe.java
@@ -0,0 +1,13 @@
+package recipes;
+
+import lombok.Data;
+
+@Data
+public class Recipe {
+
+    private String name;
+    private String description;
+    private String ingredients;
+    private String directions;
+}
+
